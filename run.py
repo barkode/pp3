6198eb171188838b9b643f55947f605cd7cdad55
barkode <konstantinks@gmail.com>
2024-07-22 15:37:27 +0100

    Add authorization with the google sheets

--- run.py
+++ run.py
@@ -8,6 +8,20 @@ from os import name, system
 # import sleep to show output for some time period
 from time import sleep
 
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+
 
 # define our clear function
 def clear():
