e0cb3e07ccf4890bdf887219985fe9fbd56ef216
barkode <konstantinks@gmail.com>
2024-07-27 20:23:59 +0100

    refactor: split the code to each logic file

--- run.py
+++ run.py
@@ -1,140 +1,35 @@
 # import from rich library for draw tables
-import pyfiglet
+
 from rich import print as rprint
-from rich.table import Table
 
 from gsheets_api import (
     add_task,
     add_task_page,
     add_user_to_base,
     check_edit_enter,
-    check_user_name,
-    check_user_name_entering,
-    check_user_password,
     create_user_tasks_page,
     delete_task,
     edit_task,
     show_tasks,
 )
+from ui import (
+    LOGO,
+    SLEEP_TIME,
+    log_in_screen,
+    print_logo,
+    print_tasks,
+    sign_in_screen,
+    welcome_screen,
+)
 from utils import (
     clear,
     close_app,
     hash_password,
-    hide_user_pass,
     print_text,
     sleep,
 )
 
-# Default user name
 user_name = "Dear User"
-LOGO = "stodo"
-SLEEP_TIME = 2
-
-
-def print_tasks(tasks_lst: list):
-    """Print the table with tasks. Used the Rich library."""
-    rprint(f"[green]Hello, {user_name}. Your Tasks.[/green]")
-    table = Table(
-        show_header=True,
-        header_style="bold magenta",
-    )
-    table.add_column("Number", style="dim", justify="center", width=12)
-    table.add_column("Tasks", justify="center")
-    table.add_column("Date", justify="center")
-    for count, el in enumerate(tasks_lst, start=1):
-        table.add_row(f"{count:02}", f'{el["task"]}', f'{el["time_stamp"]}')
-    rprint(table)
-
-
-def sign_in_screen():
-    """Called when the user is not registered in the system.
-    Shows a screen inviting the user to register with the system.
-    """
-    clear()
-    while True:
-        clear()
-        print_logo(LOGO)
-        print_text("Enter your login:", "yellow")
-        u_login = input("Login: ")
-        res = check_user_name_entering(u_login)
-        if not res["bool"]:
-            print_text(f"{res['msg']}", "magenta")
-            sleep(SLEEP_TIME)
-            continue
-        res = check_user_name(res["msg"])
-        if not res["bool"]:
-            print_text(f"{res['msg']}", "magenta")
-            sleep(SLEEP_TIME)
-            continue
-        return res["msg"]
-
-
-def log_in_screen():
-    """Shows the user login screen.
-    Checks if the user and password exist in the base.
-    """
-    while True:
-        clear()
-        print_logo(LOGO)
-        print_text("Input your login.", "yellow")
-        u_login = input("Login: ")
-        res = check_user_name_entering(u_login)
-        if not res["bool"]:
-            print(f"{res['msg']}")
-            sleep(SLEEP_TIME)
-            continue
-        print_text("Input your password.", "yellow")
-        u_pwd = hide_user_pass()
-        h_pwd = hash_password(u_pwd)
-        check = check_user_password(u_login, h_pwd)
-        if check["bool"]:
-            return u_login
-        else:
-            print_text(check["msg"], "magenta")
-            sleep(SLEEP_TIME)
-
-
-def print_logo(text: str):
-    """Print the ascii app logo"""
-    ascii_art = pyfiglet.figlet_format(text, font="banner3-D")
-    print(ascii_art)
-
-
-def welcome_screen(user_name: str) -> dict:
-    """
-    The function checks whether the user is in the system.
-    If the user is logged in, it returns True.
-    If omitted, the function returns False.
-    If a character different from N or Y is entered,
-    an error is displayed to the user.
-    """
-    try:
-        while True:
-            clear()
-            print_logo(LOGO)
-            rprint("[bold red]Are you registered?[/bold red]")
-            print("")
-            print_text("Y for existing user", "yellow")
-            print_text("N for a new user", "yellow")
-            print("")
-            is_register = input("Enter: ")
-            if is_register in "yY" and len(is_register) != 0:
-                return {
-                    "bool": True,
-                    "msg": "Greetings. Welcome back. Please enter your name.",
-                }
-            elif is_register in "nN" and len(is_register) != 0:
-                return {"bool": False, "msg": "You need to register."}
-            else:
-                print_text(
-                    "Wrong answer. Please enter Y or N.", style="magenta"
-                )
-                sleep(SLEEP_TIME)
-    except KeyboardInterrupt:
-        clear()
-        print_logo(LOGO)
-        print_text(f"Goog bye {user_name}", "orange3")
-        close_app()
 
 
 def main():
@@ -149,7 +44,7 @@ def main():
             user_name = log_in_screen()
         else:
             user_name = sign_in_screen()
-            print_text("Enter your password: ", "yellow")
+            rprint("[yellow]Enter your password: [/yellow]")
             user_pw = input("Password: ")
             hashed_pw = hash_password(user_pw)
             add_user_to_base(user_name, hashed_pw)
@@ -177,8 +72,6 @@ def main():
                 break
             elif answer in "aA":
                 clear()
-                # rprint("Enter task text :")
-                # input_task = input("Task text: ")
                 new_task = add_task_page(user_name)
                 add_task(user_name, new_task)
                 sleep(SLEEP_TIME)
@@ -188,13 +81,13 @@ def main():
                 print_tasks(all_tasks)
                 sleep(SLEEP_TIME)
             elif answer in "eE":
-                tsk_num = input("Enter number of task to edit:")
+                rprint("[yellow]Enter task number: [/yellow]", end=" ")
+                tsk_num = input()
                 res = check_edit_enter(user_name, tsk_num)
-
                 if res["bool"]:
                     edit_task(user_name, res["msg"])
                 else:
-                    rprint(f"{res['msg']}")
+                    rprint(f"[magenta]{res['msg']}[/magenta]")
                 sleep(SLEEP_TIME)
             elif answer in "dD":
                 tsk_num = input("Enter task ID: ")
---
+++ ui.py
@@ -0,0 +1,128 @@
+import pyfiglet
+from rich import print as rprint
+from rich.table import Table
+
+from gsheets_api import (
+    check_user_name,
+    check_user_name_entering,
+    check_user_password,
+)
+from utils import (
+    clear,
+    close_app,
+    hash_password,
+    hide_user_pass,
+    print_text,
+    sleep,
+)
+
+# Default parameters
+user_name = "Dear User"
+LOGO = "stodo"
+SLEEP_TIME = 2
+
+
+def welcome_screen(user_name: str) -> dict:
+    """
+    The function checks whether the user is in the system.
+    If the user is logged in, it returns True.
+    If omitted, the function returns False.
+    If a character different from N or Y is entered,
+    an error is displayed to the user.
+    """
+    try:
+        while True:
+            clear()
+            print_logo(LOGO)
+            rprint("[bold red]Are you registered?[/bold red]")
+            print("")
+            print_text("Y for existing user", "yellow")
+            print_text("N for a new user", "yellow")
+            print("")
+            is_register = input("Enter: ")
+            if is_register in "yY" and len(is_register) != 0:
+                return {
+                    "bool": True,
+                    "msg": "Greetings. Welcome back. Please enter your name.",
+                }
+            elif is_register in "nN" and len(is_register) != 0:
+                return {"bool": False, "msg": "You need to register."}
+            else:
+                print_text(
+                    "Wrong answer. Please enter Y or N.", style="magenta"
+                )
+                sleep(SLEEP_TIME)
+    except KeyboardInterrupt:
+        clear()
+        print_logo(LOGO)
+        print_text(f"Goog bye {user_name}", "orange3")
+        close_app()
+
+
+def print_tasks(tasks_lst: list):
+    """Print the table with tasks. Used the Rich library."""
+    rprint(f"[green]Hello, {user_name}. Your Tasks.[/green]")
+    table = Table(
+        show_header=True,
+        header_style="bold magenta",
+    )
+    table.add_column("Number", style="dim", justify="center", width=12)
+    table.add_column("Tasks", justify="center")
+    table.add_column("Date", justify="center")
+    for count, el in enumerate(tasks_lst, start=1):
+        table.add_row(f"{count:02}", f'{el["task"]}', f'{el["time_stamp"]}')
+    rprint(table)
+
+
+def sign_in_screen():
+    """Called when the user is not registered in the system.
+    Shows a screen inviting the user to register with the system.
+    """
+    clear()
+    while True:
+        clear()
+        print_logo(LOGO)
+        print_text("Enter your login:", "yellow")
+        u_login = input("Login: ")
+        res = check_user_name_entering(u_login)
+        if not res["bool"]:
+            print_text(f"{res['msg']}", "magenta")
+            sleep(SLEEP_TIME)
+            continue
+        res = check_user_name(res["msg"])
+        if not res["bool"]:
+            print_text(f"{res['msg']}", "magenta")
+            sleep(SLEEP_TIME)
+            continue
+        return res["msg"]
+
+
+def print_logo(text: str):
+    """Print the ascii app logo"""
+    ascii_art = pyfiglet.figlet_format(text, font="banner3-D")
+    print(ascii_art)
+
+
+def log_in_screen():
+    """Shows the user login screen.
+    Checks if the user and password exist in the base.
+    """
+    while True:
+        clear()
+        print_logo(LOGO)
+        print_text("Input your login.", "yellow")
+        u_login = input("Login: ")
+        res = check_user_name_entering(u_login)
+        if not res["bool"]:
+            print(f"{res['msg']}")
+            sleep(SLEEP_TIME)
+            continue
+        print_text("Input your password.", "yellow")
+        u_pwd = hide_user_pass()
+        h_pwd = hash_password(u_pwd)
+        check = check_user_password(u_login, h_pwd)
+        if check["bool"]:
+            return u_login
+        else:
+            print_text(check["msg"], "magenta")
+            sleep(SLEEP_TIME)
